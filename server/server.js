0686e4db9563d30836be47aca1f0068e193435ff
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2020-10-30 14:37:27 -0500

    Added  changes deploy two package.json client and server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const express = require('express');
-
+require('dotenv').config({ path: '../.env' });
 const app = express();
-app.use(express.static(__dirname + '/dist/client'));
-require('dotenv').config();
+app.use(express.static(__dirname + '/client'));
+
 
 
 app.get('/*', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'dist/client', 'index.html'));
+    return res.sendFile(path.join(__dirname, '/client', 'index.html'));
 });
 
+console.log(process.env.PORT);
 
 app.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
