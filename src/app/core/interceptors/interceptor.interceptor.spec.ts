5e8dc7c33b0ca66ffa3086eda7d683a411c602d8
Juan Falcón <JUANCANEN_10_@HOTMAIL.COM>
2020-11-25 01:19:25 -0500

    Added unit test interceptor

--- src/app/core/interceptors/interceptor.interceptor.spec.ts
+++ src/app/core/interceptors/interceptor.interceptor.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-
-import { InterceptorInterceptor } from './interceptor.interceptor';
+import { InterceptorInterceptor as Interceptor } from './interceptor.interceptor';
+import { PortalService } from '../services/portal.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 describe('InterceptorInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      InterceptorInterceptor
+  let interceptor: Interceptor;
+  let service: PortalService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        PortalService,
+         {
+           provide: HTTP_INTERCEPTORS,
+           useClass: Interceptor,
+           multi: true,
+         },
+        Interceptor
       ]
-  }));
+    });
+    interceptor = TestBed.inject(Interceptor);
+    service = TestBed.inject(PortalService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const interceptor: InterceptorInterceptor = TestBed.inject(InterceptorInterceptor);
+
     expect(interceptor).toBeTruthy();
   });
+
+  it('should added header in request', () => {
+    service.getPortal().subscribe((resp) => {
+      expect(resp).toBeTruthy();
+    });
+    const httpRequest = httpMock.expectOne(`${environment.backendUrl}/portal`);
+    expect(httpRequest.request.headers.has('token')).toEqual(true);
+
+  });
+
+
+
 });
